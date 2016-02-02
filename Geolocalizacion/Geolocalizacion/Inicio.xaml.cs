d8fb2f82e7be7ee2099fec319060afb4c0602165
JCQuiroga <josecquiroga@gmail.com>
2016-02-02 12:49:45 +0100

    -:||:-

---
+++ Geolocalizacion/Geolocalizacion/Inicio.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Geolocator;
+using Xamarin.Forms;
+
+namespace Geolocalizacion
+{
+    public partial class Inicio : ContentPage
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            var loc = CrossGeolocator.Current;
+            var pos = await loc.GetPositionAsync();
+
+            if (pos == null)
+            {
+                await this.DisplayAlert("Error", "No recibo la posicion", "OK");
+            }
+            TxtLat.Text = pos.Latitude.ToString();
+            TxtLong.Text = pos.Longitude.ToString();
+        }
+    }
+}
